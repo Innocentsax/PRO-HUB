a7ec1c05f8079abb829d388d2c10d4ae0bd8c712
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-22 15:33:58 +0100

    User Impl

---
+++ src/main/java/dev/Innocent/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package dev.Innocent.Service.Impl;
+
+import dev.Innocent.Service.UserService;
+import dev.Innocent.config.JwtProvider;
+import dev.Innocent.model.User;
+import dev.Innocent.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findUserProfileByJwt(String jwt) throws Exception {
+        String email = JwtProvider.getEmailFromToken(jwt);
+        return findUserByEmail(email);
+    }
+
+    @Override
+    public User findUserByEmail(String email) throws Exception {
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new Exception("User not found");
+        }
+        return user;
+    }
+
+    @Override
+    public User findUserById(Long userId) throws Exception {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if(optionalUser.isEmpty()){
+            throw new Exception("User not found");
+        }
+        return optionalUser.get();
+    }
+
+    @Override
+    public User updateUsersProjectSize(User user, int number) {
+        user.setProjectSize(user.getProjectSize()+number);
+        return userRepository.save(user);
+    }
+}
