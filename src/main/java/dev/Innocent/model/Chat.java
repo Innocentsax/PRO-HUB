c79eb62714167090ca0958e9e9ed4c33b81c010c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-22 15:32:34 +0100

    Chat

---
+++ src/main/java/dev/Innocent/model/Chat.java
@@ -0,0 +1,31 @@
+package dev.Innocent.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @OneToOne
+    private Project project;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Message> message;
+
+    @ManyToMany
+    private List<User> user = new ArrayList<>();
+}
