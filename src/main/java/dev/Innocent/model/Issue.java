77a99a3ab8d9a5c1551257b1bd9247e0ae5faf7c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-22 15:33:07 +0100

    Issues

--- src/main/java/dev/Innocent/model/Issue.java
+++ src/main/java/dev/Innocent/model/Issue.java
@@ -1,9 +1,14 @@
 package dev.Innocent.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -13,7 +18,22 @@ public class Issue {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private Long projectID;
+    private String priority;
+    private LocalDate dueDate;
+    private List<String> tags = new ArrayList<>();
 
     @ManyToOne
     private User assignee;
+
+    @JsonIgnore
+    @ManyToOne
+    private Project project;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 }
