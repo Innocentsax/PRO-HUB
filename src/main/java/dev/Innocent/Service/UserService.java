8b7a9fc393d9d3c1d47f099ec0a96a6d55bdda44
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-22 15:33:54 +0100

    User Impl

--- src/main/java/dev/Innocent/Service/UserService.java
+++ src/main/java/dev/Innocent/Service/UserService.java
@@ -1,4 +1,10 @@
 package dev.Innocent.Service;
 
+import dev.Innocent.model.User;
+
 public interface UserService {
+    User findUserProfileByJwt(String jwt) throws Exception;
+    User findUserByEmail(String email) throws Exception;
+    User findUserById(Long userId) throws Exception;
+    User updateUsersProjectSize(User user, int number);
 }
