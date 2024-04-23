a3d1861261ecb446398dde4cc08f4a32ca5bdea9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-23 11:48:18 +0100

    Issues Creation

--- src/main/java/dev/Innocent/Service/Impl/IssueServiceImpl.java
+++ src/main/java/dev/Innocent/Service/Impl/IssueServiceImpl.java
@@ -2,7 +2,11 @@ package dev.Innocent.Service.Impl;
 
 import dev.Innocent.DTO.request.IssueRequest;
 import dev.Innocent.Service.IssueService;
+import dev.Innocent.Service.ProjectService;
+import dev.Innocent.Service.UserService;
 import dev.Innocent.model.Issue;
+import dev.Innocent.model.Project;
+import dev.Innocent.model.User;
 import dev.Innocent.repository.IssueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,29 +17,43 @@ import java.util.Optional;
 @Service
 public class IssueServiceImpl implements IssueService {
     private IssueRepository issueRepository;
+    private ProjectService projectService;
+    private UserService userService;
 
     @Autowired
-    public IssueServiceImpl(IssueRepository issueRepository) {
+    public IssueServiceImpl(IssueRepository issueRepository,
+                            ProjectService projectService, UserService userService) {
         this.issueRepository = issueRepository;
+        this.projectService = projectService;
+        this.userService = userService;
     }
-
     @Override
-    public Optional<Issue> getIssueById(Long issueId) throws Exception {
+    public Issue getIssueById(Long issueId) throws Exception {
         Optional<Issue> issue = issueRepository.findById(issueId);
         if(issue.isPresent()){
-            return issue;
+            return issue.get();
         }
         throw new Exception("No issues found with issueId " + issueId);
     }
 
     @Override
     public List<Issue> getIssueByProjectId(Long projectId) throws Exception {
-        return null;
+        return issueRepository.findByProjectID(projectId);
     }
 
     @Override
-    public Issue createIssue(IssueRequest issue, Long userId) throws Exception {
-        return null;
+    public Issue createIssue(IssueRequest issueRequest, User user) throws Exception {
+        Project project = projectService.getProjectById(issueRequest.getProjectID());
+
+        Issue issue = new Issue();
+        issue.setTitle(issueRequest.getTitle());
+        issue.setDescription(issueRequest.getDescription());
+        issue.setStatus(issueRequest.getStatus());
+        issue.setProjectID(issueRequest.getProjectID());
+        issue.setPriority(issueRequest.getPriority());
+        issue.setDueDate(issueRequest.getDueDate());
+        issue.setProject(project);
+        return issueRepository.save(issue);
     }
 
     @Override
@@ -44,8 +62,9 @@ public class IssueServiceImpl implements IssueService {
     }
 
     @Override
-    public String deleteIssue(Long issueId, Long userId) throws Exception {
-        return null;
+    public void deleteIssue(Long issueId, Long userId) throws Exception {
+        getIssueById(issueId);
+        issueRepository.deleteById(issueId);
     }
 
     @Override
@@ -54,12 +73,17 @@ public class IssueServiceImpl implements IssueService {
     }
 
     @Override
-    public Issue addUserToIssue(Long issueId, Long userId) {
-        return null;
+    public Issue addUserToIssue(Long issueId, Long userId) throws Exception {
+        User user = userService.findUserById(userId);
+        Issue issue = getIssueById(issueId);
+        issue.setAssignee(user);
+        return issueRepository.save(issue);
     }
 
     @Override
-    public Issue updateStatus(Long issueId, String status) {
-        return null;
+    public Issue updateStatus(Long issueId, String status) throws Exception {
+        Issue issue = getIssueById(issueId);
+        issue.setStatus(status);
+        return issueRepository.save(issue);
     }
 }
