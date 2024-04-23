0c99c0516fbfb54b3e1c98fbbf2b837dc9b72bab
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-23 11:47:22 +0100

    Comment creation

---
+++ src/main/java/dev/Innocent/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByIssueId(Long issueId);
+}
