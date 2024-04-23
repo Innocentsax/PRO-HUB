84b255e9663fe5cbcb62912f5394c8b9d4a77c9a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-23 11:48:01 +0100

    Issues Creation

---
+++ src/main/java/dev/Innocent/DTO/IssueDTO.java
@@ -0,0 +1,29 @@
+package dev.Innocent.DTO;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.Innocent.model.Project;
+import dev.Innocent.model.User;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IssueDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private Long projectID;
+    private String priority;
+    private LocalDate dueDate;
+    private List<String> tags = new ArrayList<>();
+    private User assignee;
+    private Project project;
+}
