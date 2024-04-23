992ef535d6bf99408308501d5fdef9c9c03b3af8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-23 11:48:14 +0100

    Issues Creation

--- src/main/java/dev/Innocent/Service/IssueService.java
+++ src/main/java/dev/Innocent/Service/IssueService.java
@@ -2,18 +2,19 @@ package dev.Innocent.Service;
 
 import dev.Innocent.DTO.request.IssueRequest;
 import dev.Innocent.model.Issue;
+import dev.Innocent.model.User;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IssueService {
-    Optional<Issue> getIssueById(Long issueId) throws Exception;
+    Issue getIssueById(Long issueId) throws Exception;
     List<Issue> getIssueByProjectId(Long projectId) throws Exception;
-    Issue createIssue(IssueRequest issue, Long userId) throws Exception;
+    Issue createIssue(IssueRequest issueRequest, User user) throws Exception;
     Optional<Issue> updateIssue(Long issueId, IssueRequest updateIssue, Long userId) throws Exception;
-    String deleteIssue(Long issueId, Long userId) throws Exception;
+    void deleteIssue(Long issueId, Long userId) throws Exception;
     List<Issue> getIssueByAssigneeId(Long assignId);
-    Issue addUserToIssue(Long issueId, Long userId);
-    Issue updateStatus(Long issueId, String status);
+    Issue addUserToIssue(Long issueId, Long userId) throws Exception;
+    Issue updateStatus(Long issueId, String status) throws Exception;
 
 }
