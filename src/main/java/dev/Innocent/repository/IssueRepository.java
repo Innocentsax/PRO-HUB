e58d14f823e8063a0a24ac9b9ce65496823a5151
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-23 11:48:11 +0100

    Issues Creation

--- src/main/java/dev/Innocent/repository/IssueRepository.java
+++ src/main/java/dev/Innocent/repository/IssueRepository.java
@@ -3,5 +3,8 @@ package dev.Innocent.repository;
 import dev.Innocent.model.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+    public List<Issue> findByProjectID(Long id);
 }
