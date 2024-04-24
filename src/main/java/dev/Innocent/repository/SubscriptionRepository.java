d05d1b939d1451070d54acd34095de266e53b768
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-24 07:45:24 +0100

    Subscription feat

---
+++ src/main/java/dev/Innocent/repository/SubscriptionRepository.java
@@ -0,0 +1,8 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    Subscription findByUserId(Long userId);
+}
