8f2d8b54df15acd173726d922d37b530f82c6806
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-24 07:44:20 +0100

    Message feat

---
+++ src/main/java/dev/Innocent/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByChatIdOrderByCreatedAtAsc(Long chatId);
+}
