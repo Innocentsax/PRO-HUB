6a34332bca65b41984834bf4a71f97ae2c82b982
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-25 10:16:03 +0100

    Hot fix

--- src/main/java/dev/Innocent/model/User.java
+++ src/main/java/dev/Innocent/model/User.java
@@ -1,6 +1,7 @@
 package dev.Innocent.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +19,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String fullName;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String email;
     private int projectSize;
